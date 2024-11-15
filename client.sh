1f53bc06dda49799e0289a9e5c84724de2adf9e1
ivan <ivan.garcia@enti.cat>
2024-11-15 09:55:42 +0100

    Avances en el protocolo

--- client.sh
+++ client.sh
@@ -5,7 +5,7 @@ PORT="2022"
 
 echo "1. ENVIO DE CABECERA"
 
-echo "MAM" | nc 127.0.0.1 $PORT
+echo "DMAM" | nc 127.0.0.1 $PORT
 
 DATA=`nc -l $PORT`
 
@@ -14,3 +14,15 @@ then
 	echo "ERROR 1: El header se envió incorrectamente"
 	exit 1
 fi
+
+echo "4. ENVIANDO EL FILE_NAME"
+
+FILE_NAME="dragon.txt"
+
+echo "FILE_NAME $FILE_NAME" | nc localhost $PORT
+
+DATA=`nc -l $PORT`
+
+if [ "$DATA" !="OK_FILE_NAME" ]
+then
+	echo "ERROR 2: 
